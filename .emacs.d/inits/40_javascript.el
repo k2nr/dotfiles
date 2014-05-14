f4d73b85cba7d275ddb67b965df271bd9df2dc54
Kazunori Kajihiro <likerichie@gmail.com>
2014-05-14 21:36:35 +0900

    js2-mode

--- .emacs.d/inits/40_javascript.el
+++ .emacs.d/inits/40_javascript.el
@@ -1,3 +1,5 @@
-;;(require 'js2-mode)
-;;(setq js2-mirror-mode t)
-;;(setq js2-enter-indents-newline t)
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq js2-mirror-mode t)
+(setq js2-enter-indents-newline t)
+(setq-default js2-basic-offset 2)
