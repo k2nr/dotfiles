fcb95d5c0dfc01d5b9f00a39620ca384edf0c663
Kazunori Kajihiro <likerichie@gmail.com>
2014-05-17 15:30:01 +0900

    add lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,8 @@
 {:user {:plugins [[lein-exec "0.3.3"]
                   [lein-ancient "0.5.5"]
                   [lein-try "0.4.1"]
+                  [lein-midje "3.1.3"]
+                  [cider/cider-nrepl "0.6.0"]
                   ]
         }
  }
