2de5f41552ba0cb53481d7a9a95935a0cbcd3f4b
Kazunori Kajihiro <likerichie@gmail.com>
2014-05-17 15:27:22 +0900

    add exec-path-from-shell

--- .emacs.d/inits/00_exec-path.el
+++ .emacs.d/inits/00_exec-path.el
@@ -1,16 +1,2 @@
-(dolist (dir (list
-              "/sbin"
-              "/usr/sbin"
-              "/bin"
-              "/usr/bin"
-              "/opt/local/bin"
-              "/sw/bin"
-              "/usr/local/bin"
-              (expand-file-name "~/bin")
-              (expand-file-name "~/.emacs.d/bin")
-              (expand-file-name "~/Library/Haskell/bin")
-              (expand-file-name "~/.cabal/bin")
-              ))
- (when (and (file-exists-p dir) (not (member dir exec-path)))
-   (setenv "PATH" (concat dir ":" (getenv "PATH")))
-   (setq exec-path (append (list dir) exec-path))))
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
