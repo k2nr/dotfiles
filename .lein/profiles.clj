8cdc813ba86f284ee10096a0e93b3968a89308db
Kazunori Kajihiro <likerichie@gmail.com>
2014-08-26 07:56:36 +0900

    update cire-nrepl version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,7 @@
                   [lein-ancient "0.5.5"]
                   [lein-try "0.4.1"]
                   [lein-midje "3.1.3"]
-                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.7.0"]
                   ]
         :injections [(require '[clojure.pprint :refer :all])
                      (require '[clojure.repl :refer :all])]}
