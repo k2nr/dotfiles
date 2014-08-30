7a6fde7d4a2bcb3d1520440296df303655242883
Kazunori Kajihiro <likerichie@gmail.com>
2014-08-30 09:11:34 +0900

    update profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,8 @@
 {:user {:plugins [[lein-exec "0.3.4"]
                   [lein-ancient "0.5.5"]
-                  [lein-try "0.4.1"]
+                  [lein-try "0.4.3"]
                   [lein-midje "3.1.3"]
-                  [cider/cider-nrepl "0.7.0"]
+                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]
                   ]
         :injections [(require '[clojure.pprint :refer :all])
                      (require '[clojure.repl :refer :all])]}
