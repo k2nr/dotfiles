49d2e8024ed0a20ad275ccb6b387bb94aae5eec5
Kazunori Kajihiro <likerichie@gmail.com>
2014-09-18 05:48:46 +0900

    add gofmt-before-save

--- .emacs.d/inits/40_go.el
+++ .emacs.d/inits/40_go.el
@@ -2,3 +2,6 @@
 
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "M-.") 'godef-jump)))
+
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook #'gofmt-before-save)
