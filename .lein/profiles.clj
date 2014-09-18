e98039ec7e9c89c63c9922fa636b847c9c2fe2cc
Kazunori Kajihiro <likerichie@gmail.com>
2014-09-18 12:37:07 +0900

    add slamhound

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,11 @@
-{:user {:plugins [[lein-exec "0.3.4"]
+{:user {:dependencies [[slamhound "1.5.5"]]
+        :plugins [[lein-exec "0.3.4"]
                   [lein-ancient "0.5.5"]
                   [lein-try "0.4.3"]
                   [lein-midje "3.1.3"]
                   [cider/cider-nrepl "0.8.0-SNAPSHOT"]
                   ]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :injections [(require '[clojure.pprint :refer :all])
                      (require '[clojure.repl :refer :all])]}
  }
