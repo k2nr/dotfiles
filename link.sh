f77482e69a91153ba2f78180d2fa90418c5baaab
Kazunori Kajihiro <likerichie@gmail.com>
2014-09-20 11:01:03 +0900

    don't link .DS_Store

--- link.sh
+++ link.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+
 for f in .*
 do
-  if test $f != "." && test $f != ".."; then
+  if test $f != "." && test $f != ".." && test $f != ".DS_Store"; then
     echo replacing $f
     rm -rf $HOME/$f
     ln -s "`pwd`/$f" $HOME/$f
