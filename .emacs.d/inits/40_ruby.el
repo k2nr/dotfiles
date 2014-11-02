bfae30e1119e331874287063b03cc07266f31a63
Kazunori Kajihiro <likerichie@gmail.com>
2014-11-02 21:01:18 +0900

    update emacs ruby settings

--- .emacs.d/inits/40_ruby.el
+++ .emacs.d/inits/40_ruby.el
@@ -1,10 +1,4 @@
-(require 'inf-ruby)
-(require 'rinari)
-(global-rinari-mode +1)
-
-(require 'rvm)
-(rvm-use-default) ;; use rvm's default ruby for the current Emacs session
-
+;; indent
 (dolist (spec '(("\\.rb$"          . ruby-mode)
                 ("[vV]agrantfile$" . ruby-mode)
                 ("[gG]emfile$"     . ruby-mode)
@@ -15,15 +9,30 @@
                 ("\\.builder$"     . ruby-mode)))
   (add-to-list 'auto-mode-alist spec))
 
+(defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+  (rbenv-use-corresponding))
+
 ;; robe
-(require 'robe)
-(add-hook 'ruby-mode-hook (lambda (robe-mode)))
+(add-hook 'ruby-mode-hook 'robe-mode)
+(add-hook 'robe-mode-hook 'ac-robe-setup)
 
 ;; inf-ruby
-(require 'inf-ruby)
 (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
 
-;; indent
+;; rinari
+(defvar my-rinari-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "m") 'rinari-find-model)
+    (define-key map (kbd "v") 'rinari-find-view)
+    (define-key map (kbd "c") 'rinari-find-controller)
+    (define-key map (kbd "r") 'rinari-find-routes)
+    (define-key map (kbd "t") 'rinari-find-test)
+    (define-key map (kbd "s") 'rinari-find-rspec)
+    map))
+(add-hook 'ruby-mode-hook (lambda ()
+                              (evil-define-key 'normal rinari-minor-mode-map (kbd ", r") my-rinari-map)))
+(global-rinari-mode +1)
+
 (setq ruby-deep-indent-paren-style nil)
 
 (modify-syntax-entry ?_ "w" ruby-mode-syntax-table)
