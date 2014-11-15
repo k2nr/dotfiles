e712b86acbf3a30a679ff53aa4476c99be995488
Kazunori Kajihiro <likerichie@gmail.com>
2014-11-15 20:03:29 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ brew bundle
 
 #install emacs cask
 curl -fsSkL https://raw.github.com/cask/cask/master/go | python
+
+# Go Packages
+go get -u github.com/nsf/gocode
+go get -u code.google.com/p/rog-go/exp/cmd/godef
+go get -u code.google.com/p/go.tools/cmd/goimports
