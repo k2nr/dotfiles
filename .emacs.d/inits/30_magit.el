fa9ce2c954b6302f1ff16fe5b5538310c4979743
Kazunori Kajihiro <likerichie@gmail.com>
2015-01-21 00:29:16 +0900

    update magit settings

--- .emacs.d/inits/30_magit.el
+++ .emacs.d/inits/30_magit.el
@@ -1,7 +1,7 @@
 (defadvice magit-status (around magit-fullscreen activate)
   (window-configuration-to-register :magit-fullscreen)
   ad-do-it
-  ;(delete-other-windows)
+  (delete-other-windows)
   )
 
 (defun magit-quit-session ()
