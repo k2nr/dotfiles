febc64e0e3c2655ce4b20d8278b048b827e61968
Kazunori Kajihiro <likerichie@gmail.com>
2015-01-21 00:28:58 +0900

    change web mode offset values

--- .emacs.d/inits/20_web-mode.el
+++ .emacs.d/inits/20_web-mode.el
@@ -7,7 +7,9 @@
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
 
 (defun web-mode-hook ()
+  (setq web-mode-markup-indent-offset 2)
   (setq web-mode-html-offset   2)
+  (setq web-mode-code-indent-offset 2)
   (setq web-mode-css-offset    2)
   (setq web-mode-script-offset 2)
   (setq web-mode-php-offset    2)
