81825bb585e0258757db22b2fd1d3d3b475acaed
Kazunori Kajihiro <likerichie@gmail.com>
2015-01-22 23:13:37 +0900

    upgrade profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
         :plugins [[lein-exec "0.3.4"]
-                  [lein-ancient "0.5.5"]
+                  [lein-ancient "0.6.1"]
                   [lein-try "0.4.3"]
                   [lein-midje "3.1.3"]
                   [cider/cider-nrepl "0.8.2"]
