724f88d6663b57790cf975fa51ab3a3cc0d90f92
Kazunori Kajihiro <likerichie@gmail.com>
2015-06-20 11:27:55 +0900

    Update clojure profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user {:dependencies [[slamhound "1.5.5"]]
-        :plugins [[lein-exec "0.3.4"]
-                  [lein-ancient "0.6.4"]
+        :plugins [[lein-exec "0.3.5"]
+                  [lein-ancient "0.6.7"]
                   [lein-try "0.4.3"]
                   [lein-midje "3.1.3"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [refactor-nrepl "0.3.0-SNAPSHOT"]
+                  [refactor-nrepl "1.0.5"]
                   ]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :injections [(require '[clojure.pprint :refer :all])
