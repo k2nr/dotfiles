c2b4beb0e76f5281c2f8cf1beef7142537be5554
Kazunori Kajihiro <likerichie@gmail.com>
2015-12-28 18:59:53 +0900

    Add spacemacs installation

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew bundle
 
-#install emacs cask
-curl -fsSkL https://raw.github.com/cask/cask/master/go | python
-
 # Go Packages
 go get -u github.com/nsf/gocode
 go get -u code.google.com/p/rog-go/exp/cmd/godef
 go get -u code.google.com/p/go.tools/cmd/goimports
+
+# Spacemacs
+git clone -b develop --recursive https://github.com/syl20bnr/spacemacs ./.emacs.d
