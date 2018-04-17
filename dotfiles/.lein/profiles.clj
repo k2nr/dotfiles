b2a54095576de967f52b9f38c80a80c0636fac11
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2018-04-18 00:21:41 +0900

    Update

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -1,5 +1,7 @@
-{:user {:plugins [[lein-ancient "0.6.10"]
+{:user {:plugins [[lein-ancient "0.6.15"]
                   [lein-try "0.4.3"]
+                  [jonase/eastwood "0.2.5"]
+                  [lein-kibit "0.1.6"]
                   ]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :injections [(require '[clojure.pprint :refer :all])
